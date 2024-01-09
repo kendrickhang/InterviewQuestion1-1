00296878f9f0213202c697ea0cd983b9a221cd79
EBoydGRC <EBoydGRC@noreply.github.com>
2024-01-09 10:11:17 -0800

    isUnique is running.

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!\n");
+        System.out.println("Is 'test' unique: " + isUnique("test"));
+        System.out.println("is 'steak' unique: "+ isUnique("steak"));
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
@@ -16,11 +18,12 @@ public class Main {
     }
     public static boolean isUnique(String inputString)
     {
-        //HashSet textString = new HashSet();
-        //for(int i = 0; i < inputString.length(); i++)
-        //{
-
-        //}
+        HashSet testString = new HashSet();
+        for(int i = 0; i < inputString.length(); i++)
+        {
+            if (!(testString.add(inputString.charAt(i))))
+                return false;
+        }
         return true;
     }
 }
